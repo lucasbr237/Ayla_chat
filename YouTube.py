53ea38b6c7ac20033466db0810b6febcd27b5527
Lukz770 <72799605+lucasbr237@users.noreply.github.com>
2024-06-01 12:00:17 -0300

    Create YouTube.py

---
+++ YouTube.py
@@ -0,0 +1,60 @@
+
+from googleapiclient.discovery import build
+f# modules/youtube.py
+#from googleapiclient.discovery import build
+
+from config import API_KEY_YOUTUBE
+
+#def get_video_info(video_id):
+#    youtube = build('youtube', 'v3', developerKey=API_KEY_YOUTUBE)
+#    request = youtube.videos().list(
+#        part="snippet, statistics",
+#        id=video_id
+#    )
+#    response = request.execute()
+#    if 'items' in response:
+#        video_info = response['items'][0]
+#        return {
+#            'title': video_info['snippet']['title'],
+#            'views': video_info['statistics']['viewCount'],
+#            'likes': video_info['statistics']['likeCount']
+#        }
+#    else:
+#        return None
+
+from googleapiclient.discovery import build
+from config import API_KEY_YOUTUBE
+
+def get_video_info(video_id):
+    youtube = build('youtube', 'v3', developerKey=API_KEY_YOUTUBE)
+    request = youtube.videos().list(
+        part="snippet, statistics",
+        id=video_id
+    )
+    response = request.execute()
+    if 'items' in response:
+        video_info = response['items'][0]
+        return {
+            'title': video_info['snippet']['title'],
+            'views': video_info['statistics']['viewCount'],
+            'likes': video_info['statistics']['likeCount']
+        }
+    else:
+        return Nonerom config import API_KEY_YOUTUBE
+
+def get_video_info(video_id):
+    youtube = build('youtube', 'v3', developerKey=API_KEY_YOUTUBE)
+    request = youtube.videos().list(
+        part="snippet, statistics",
+        id=video_id
+    )
+    response = request.execute()
+    if 'items' in response:
+        video_info = response['items'][0]
+        return {
+            'title': video_info['snippet']['title'],
+            'views': video_info['statistics']['viewCount'],
+            'likes': video_info['statistics']['likeCount']
+        }
+    else:
+        return None
